67be330a4b649eab202ef1da8441e96a81da1a71
ghost717 <zawadzki7717@interia.pl>
2024-04-16 00:36:03 +0200

    refactoring views

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,21 +1,11 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import (
-    HotelListApiView,
-    HotelDetailApiView,
-    ReservationListApiView,
-    ReservationDetailApiView,
-    RoomListApiView,
-    RoomDetailApiView,
-)
+from django.urls import path
+from .views import HotelList, HotelDetail, ReservationList, ReservationDetail, RoomList, RoomDetail
 
 urlpatterns = [
-    path('hotels/', HotelListApiView.as_view(), name='hotel-list'),
-    path('hotels/<int:hotel_id>/', HotelDetailApiView.as_view(), name='hotel-detail'),
-    
-    path('reservations/', ReservationListApiView.as_view(), name='reservation-list'),
-    path('reservations/<int:reservation_id>/', ReservationDetailApiView.as_view(), name='reservation-detail'),
-
-    path('rooms/', RoomListApiView.as_view(), name='room-list'),
-    path('rooms/<int:room_id>/', RoomDetailApiView.as_view(), name='room-detail'),
+    path('hotels/', HotelList.as_view(), name='hotel-list'),
+    path('hotels/<int:pk>/', HotelDetail.as_view(), name='hotel-detail'),
+    path('reservations/', ReservationList.as_view(), name='reservation-list'),
+    path('reservations/<int:pk>/', ReservationDetail.as_view(), name='reservation-detail'),
+    path('rooms/', RoomList.as_view(), name='room-list'),
+    path('rooms/<int:pk>/', RoomDetail.as_view(), name='room-detail'),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -1,300 +1,40 @@
-from rest_framework import viewsets
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import permissions
-from .models import Reservation, Hotel, Room
-from .serializer import ReservationSerializer, HotelSerializer, RoomSerializer
+from rest_framework import generics, permissions
+from .models import Hotel, Reservation, Room
+from .serializer import HotelSerializer, ReservationSerializer, RoomSerializer
 
-# class ReservationViewSet(viewsets.ModelViewSet):
-#     queryset = Reservation.objects.all()
-#     serializer_class = ReservationSerializer
-
-class HotelListApiView(APIView):
-    # add permission to check if user is authenticated
+# Bazowa klasa dla APIView z często używanymi metodami
+class BaseView:
     permission_classes = [permissions.IsAuthenticated]
 
-    # 1. List all
-    def get(self, request, *args, **kwargs):
-        '''
-        List all the Hotel items
-        '''
-        hotels = Hotel.objects.filter()
-        serializer = HotelSerializer(hotels, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    # 2. Create
-    def post(self, request, *args, **kwargs):
-        '''
-        Create the Hotel with given Hotel data
-        '''
-        data = {
-            'hotel': request.data.get('hotel'), 
-            'name': request.data.get('name'), 
-        }
-        serializer = HotelSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-    
-class HotelDetailApiView(APIView):
-    # add permission to check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self, hotel_id):
-        '''
-        Helper method to get the object with given hotel_id
-        '''
+    def get_object(self, model, pk):
         try:
-            return Hotel.objects.get(id=hotel_id)
-        except Hotel.DoesNotExist:
+            return model.objects.get(pk=pk)
+        except model.DoesNotExist:
             return None
 
-    # 3. Retrieve
-    def get(self, request, hotel_id, *args, **kwargs):
-        '''
-        Retrieves the Hotel with given hotel_id
-        '''
-        hotel_instance = self.get_object(hotel_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with hotel id does not exists"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
+# Widoki dla Hotel
+class HotelList(generics.ListCreateAPIView, BaseView):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
 
-        serializer = HotelSerializer(hotel_instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class HotelDetail(generics.RetrieveUpdateDestroyAPIView, BaseView):
+    queryset = Hotel.objects.all()
+    serializer_class = HotelSerializer
 
-    # 4. Update
-    def put(self, request, hotel_id, *args, **kwargs):
-        '''
-        Updates the hotel item with given hotel_id if exists
-        '''
-        hotel_instance = self.get_object(hotel_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with hotel id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        data = {
-            'hotel': request.data.get('hotel'), 
-            'name': request.data.get('name'), 
-        }
-        serializer = HotelSerializer(instance = hotel_instance, data=data, partial = True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# Widoki dla Reservation
+class ReservationList(generics.ListCreateAPIView, BaseView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
 
-    # 5. Delete
-    def delete(self, request, hotel_id, *args, **kwargs):
-        '''
-        Deletes the hotel item with given hotel_id if exists
-        '''
-        hotel_instance = self.get_object(hotel_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with hotel id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        hotel_instance.delete()
-        return Response(
-            {"res": "Object deleted!"},
-            status=status.HTTP_200_OK
-        )    
+class ReservationDetail(generics.RetrieveUpdateDestroyAPIView, BaseView):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
 
-class ReservationListApiView(APIView):
-    # add permission to check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
+# Widoki dla Room
+class RoomList(generics.ListCreateAPIView, BaseView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
 
-    # 1. List all
-    def get(self, request, *args, **kwargs):
-        '''
-        List all the Reservation items
-        '''
-        hotels = Reservation.objects.filter()
-        serializer = ReservationSerializer(hotels, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    # 2. Create
-    def post(self, request, *args, **kwargs):
-        '''
-        Create the Reservation with given Reservation data
-        '''
-        data = {
-            'reservation': request.data.get('reservation'), 
-            # 'name': request.data.get('name'), 
-        }
-        serializer = ReservationSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-    
-class ReservationDetailApiView(APIView):
-    # add permission to check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self, reservation_id):
-        '''
-        Helper method to get the object with given reservation_id
-        '''
-        try:
-            return Reservation.objects.get(id=reservation_id)
-        except Reservation.DoesNotExist:
-            return None
-
-    # 3. Retrieve
-    def get(self, request, reservation_id, *args, **kwargs):
-        '''
-        Retrieves the Reservation with given reservation_id
-        '''
-        hotel_instance = self.get_object(reservation_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with reservation id does not exists"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        serializer = ReservationSerializer(hotel_instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    # 4. Update
-    def put(self, request, reservation_id, *args, **kwargs):
-        '''
-        Updates the reservation item with given reservation_id if exists
-        '''
-        hotel_instance = self.get_object(reservation_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with reservation id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        data = {
-            'reservation': request.data.get('reservation'), 
-            # 'name': request.data.get('name'), 
-        }
-        serializer = ReservationSerializer(instance = hotel_instance, data=data, partial = True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # 5. Delete
-    def delete(self, request, reservation_id, *args, **kwargs):
-        '''
-        Deletes the reservation item with given reservation_id if exists
-        '''
-        hotel_instance = self.get_object(reservation_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with reservation id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        hotel_instance.delete()
-        return Response(
-            {"res": "Object deleted!"},
-            status=status.HTTP_200_OK
-        )    
-
-
-class RoomListApiView(APIView):
-    # add permission to check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
-
-    # 1. List all
-    def get(self, request, *args, **kwargs):
-        '''
-        List all the Room items
-        '''
-        hotels = Room.objects.filter()
-        serializer = RoomSerializer(hotels, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    # 2. Create
-    def post(self, request, *args, **kwargs):
-        '''
-        Create the Room with given Room data
-        '''
-        data = {
-            'room': request.data.get('room'), 
-            # 'name': request.data.get('name'), 
-        }
-        serializer = RoomSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-    
-class RoomDetailApiView(APIView):
-    # add permission to check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self, room_id):
-        '''
-        Helper method to get the object with given room_id
-        '''
-        try:
-            return Room.objects.get(id=room_id)
-        except Room.DoesNotExist:
-            return None
-
-    # 3. Retrieve
-    def get(self, request, room_id, *args, **kwargs):
-        '''
-        Retrieves the Room with given room_id
-        '''
-        hotel_instance = self.get_object(room_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with room id does not exists"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        serializer = RoomSerializer(hotel_instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    # 4. Update
-    def put(self, request, room_id, *args, **kwargs):
-        '''
-        Updates the room item with given room_id if exists
-        '''
-        hotel_instance = self.get_object(room_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with room id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        data = {
-            'room': request.data.get('room'), 
-            # 'name': request.data.get('name'), 
-        }
-        serializer = RoomSerializer(instance = hotel_instance, data=data, partial = True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    # 5. Delete
-    def delete(self, request, room_id, *args, **kwargs):
-        '''
-        Deletes the room item with given room_id if exists
-        '''
-        hotel_instance = self.get_object(room_id)
-        if not hotel_instance:
-            return Response(
-                {"res": "Object with room id does not exists"}, 
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        hotel_instance.delete()
-        return Response(
-            {"res": "Object deleted!"},
-            status=status.HTTP_200_OK
-        )    
-
-
-    
+class RoomDetail(generics.RetrieveUpdateDestroyAPIView, BaseView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
