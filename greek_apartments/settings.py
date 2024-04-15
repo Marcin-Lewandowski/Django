9ce1f293ec7bfe4b980bfcb28e7be669061f62d9
ghost717 <zawadzki7717@interia.pl>
2024-04-16 01:14:02 +0200

    swagger & redoc

--- greek_apartments/settings.py
+++ greek_apartments/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'greek_apartments',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg'
 ]
 
 REST_FRAMEWORK = {
--- greek_apartments/urls.py
+++ greek_apartments/urls.py
@@ -14,17 +14,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include, re_path
 from django.contrib import admin
-from django.urls import path, include
-from . import views
-from clients.views import reservation_planning, user_profile
-from hotel import urls as hotel_urls
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Documentation",
+      default_version='v1',
+      description="API documentation for all available endpoints",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="example@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    # SWAGGER
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
+    # DJANGO
     path('admin/', admin.site.urls),
     path('api/v1/', include([
-        path('', include('hotel.urls')),
-        path('', include('hotel.urls')),
         path('', include('hotel.urls')),
     ])),
 ]
